9aa87d83fb56ea1a19c93de2d067aea3a83c01d6
wahyulamani <razorwahyu1@gmail.com>
2023-07-29 11:42:46 +0800

    already deploy, setup localdb

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 8989;
 const moment = require("moment");
-const storage = require("node-sessionstorage");
 const { client } = require("./lib/mongo");
 require("dotenv").config();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const datetime = moment().format("DDMMYYYY");
+const db = client.db(process.env.DB_NAME);
+
+const urlCheck = (titik) => {
+  if (titik == process.env.LT1) {
+    return "lt1";
+  }
+  if (titik == process.env.LT2) {
+    return "lt2";
+  }
+  if (titik == process.env.LT3) {
+    return "lt3";
+  } else {
+    return undefined;
+  }
+};
 
 async function store(data) {
-  const db = client.db(process.env.DB_NAME);
   db.collection(datetime).insertOne(
     {
       lokasi: data.Lokasi,
@@ -21,40 +38,30 @@ async function store(data) {
         console.log(error);
       }
       console.log(result);
-      return "oke";
     }
   );
 }
 
-app.get("/tes", (req, res) => {
-  store();
-});
-
 app.get("/", (req, res) => {
-  let rq = req.query;
-  if (rq.titik == undefined) {
-    res.send("<h1>SCAN QRCODE!!</h1>");
+  if (urlCheck(req.query.titik) == undefined) {
+    return res.send("<h1>SCAN QRCODE!!</h1>");
   }
-  console.log(storage.getItem(datetime) + "root");
-  storage.setItem(datetime, rq.titik);
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/petugas", (req, res) => {
-  return console.log(req);
+app.post("/petugas", (req, res) => {
+  lokasi = urlCheck(req.body.url);
+  if (lokasi == undefined) {
+    return res.send("<h1>SCAN QRCODE!!</h1>");
+  }
+
   let data = {
-    Lokasi: storage.getItem(datetime),
+    Lokasi: lokasi,
     Tanggal: moment().format("DD:mm:yyyy hh:mm:ss"),
-    Petugas: req.query.petugas,
+    Petugas: req.body.petugas,
   };
   store(data);
-  res.sendFile("<h1> Berhasil! </h1>");
-});
-
-app.get("/clear", (req, res) => {
-  storage.removeItem(datetime);
-  console.log(storage.getItem(datetime) + " clear");
-  res.send("<h1> Berhasil Di delete </h1>");
+  return res.redirect("/");
 });
 
 app.listen(port, () => {
